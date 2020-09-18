c808a206103fd16a587d934b84c0b1fc999615de
Isaac Freund <ifreund@ifreund.xyz>
2020-09-19 00:53:00 +0200

    Implement some parsing

--- scanner.zig
+++ scanner.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const mem = std.mem;
+
 const c = @cImport({
     @cInclude("expat.h");
 });
@@ -8,6 +10,10 @@ const gpa = std.heap.c_allocator;
 
 const Context = struct {
     const Self = @This();
+
+    protocol: Protocol,
+    interface: *Interface,
+    message: *Message,
 
     character_data: std.ArrayList(u8) = std.ArrayList(u8).init(gpa),
 
@@ -24,7 +30,6 @@ const Protocol = struct {
 const Interface = struct {
     name: []const u8,
     version: u32,
-    since: u32,
     requests: std.ArrayList(Message) = std.ArrayList(Message).init(gpa),
     events: std.ArrayList(Message) = std.ArrayList(Message).init(gpa),
     enums: std.ArrayList(Enum) = std.ArrayList(Enum).init(gpa),
@@ -34,6 +39,7 @@ const Message = struct {
     name: []const u8,
     since: u32,
     args: std.ArrayList(Arg) = std.ArrayList(Arg).init(gpa),
+    destructor: bool,
 };
 
 const Arg = struct {
@@ -90,6 +96,63 @@ pub fn main() !void {
 
 fn start(user_data: ?*c_void, name: ?[*:0]const u8, atts: ?[*:null]?[*:0]const u8) callconv(.C) void {
     const ctx = @intToPtr(*Context, @ptrToInt(user_data));
+    handleStart(ctx, mem.span(name.?), atts.?) catch |err| {
+        std.debug.print("error reading xml: {}", .{err});
+        std.os.exit(1);
+    };
+}
+
+fn handleStart(ctx: *Context, name: []const u8, raw_atts: [*:null]?[*:0]const u8) !void {
+    const atts = struct {
+        name: ?[]const u8 = null,
+        interface: ?[]const u8 = null,
+        version: ?u32 = null,
+        since: ?u32 = null,
+        @"type": ?[]const u8 = null,
+        value: ?[]const u8 = null,
+        summary: ?[]const u8 = null,
+        @"allow-null": ?[]const u8 = null,
+        @"enum": ?[]const u8 = null,
+        bitfield: ?[]const u8 = null,
+    }{};
+
+    var i: usize = 0;
+    while (raw_atts[i]) |att| : (i += 2) {
+        inline for (@typeInfo(@Type(atts)).Struct.fields) |field| {
+            if (mem.eql(u8, field.name, mem.span(att))) {
+                const val = mem.span(raw_atts[i + 1]);
+                if (field.field_type == ?u32) {
+                    @field(atts, field.name) = try std.fmt.parseInt(u32, val, 10);
+                } else {
+                    @field(atts, field.name) = val;
+                }
+            }
+        }
+    }
+
+    if (mem.eql(name, "protocol")) {
+        ctx.protocol = Protocol{ .name = try mem.dupe(atts.name.?) };
+    } else if (mem.eql(name, "interface")) {
+        ctx.interface = try ctx.protocol.interfaces.addOne();
+        ctx.interface.* = .{
+            .name = try mem.dupe(atts.name.?),
+            .version = atts.version.?,
+        };
+    } else if (mem.eql(name, "event") or mem.eql(name, "request")) {
+        const list = if (mem.eql(name, "event")) &ctx.interface.events else &ctx.interface.requests;
+        ctx.message = try list.addOne();
+        ctx.message.* = .{
+            .name = try mem.dupe(atts.name.?),
+            .destructor = if (atts.@"type") |t| mem.eql(u8, t, "destructor") else false,
+            .since = atts.since orelse 1,
+        };
+    } else if (mem.eql(name, "arg")) {
+        // TODO
+    } else if (mem.eql(name, "enum")) {
+        // TODO
+    } else if (mem.eql(name, "entry")) {
+        // TODO
+    }
 }
 
 fn end(user_data: ?*c_void, name: ?[*:0]const u8) callconv(.C) void {
